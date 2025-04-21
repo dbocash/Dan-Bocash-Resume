2bf92f66e7cecd48e879543a2b1b34057cf2e0b2
Daniel Bocash <danielbocash@Daniels-MacBook-Air-143.local>
2025-04-21 09:43:25 -0400

    Commiting because

--- src/Components/PageContainerFile/PageContainerDefault.jsx
+++ src/Components/PageContainerFile/PageContainerDefault.jsx
@@ -3,11 +3,10 @@ import { styled, useTheme } from '@mui/material/styles';
 import { AppProvider } from '@toolpad/core/AppProvider';
 import {
   PageContainer,
-  PageHeader,
-  PageHeaderToolbar,
 } from '@toolpad/core/PageContainer';
 import Grid from '@mui/material/Grid2';
-import {Paper, Typography} from '@mui/material';
+import {Fade, Paper, Typography} from '@mui/material';
+import CustomizedTimeline from '../CustomizedTimeline';
 import Face from '../../util/Images/face.jpg'
 import './PageContainerDefault.css';
 
@@ -32,6 +31,40 @@ const Skeleton = styled('div')(({ theme, height }) => ({
   content: '" "',
 }));
 
+const introduction = (
+    <Paper>
+      <Paper className='title-paper'
+      elevation={6}
+      sx={{
+        borderRadius: "16px",
+        boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.2)",
+        textAlign: "center",
+        backgroundColor: "#1976D2",
+        color: "#fff",
+      }}>
+        <Typography variant='h4' sx={{fontWeight: "bold"}}>
+          Welcome! I'm Daniel Bocash
+        </Typography>
+      </Paper>
+      <Paper className='introduction-paper'
+        elevation={6}
+        sx={{
+          padding: 3,
+          borderRadius: "16px",
+          boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.2)",
+          textAlign: "center",
+          backgroundColor: "#1976D2",
+          color: "#fff"
+        }}>
+        <Typography variant='body1' sx={{padding: "10px", textAlign:"center"}}>
+          I'm a passionate software engineer and computer science student at the 
+          University of New Hampshire, graduating in May 2025. With a strong foundation in C, Java, Python, and C#, 
+          I have experience in software development, embedded systems, and interoperability testing.
+        </Typography>
+      </Paper>
+    </Paper>
+);
+
 export default function PageContainerBasic(props) {
   const { window } = props;
   const router = useDemoRouter('/home');
@@ -47,30 +80,50 @@ export default function PageContainerBasic(props) {
           <Grid container spacing={1}>
             <Grid size={12}>
               <Paper sx={{p: 2, width: '100%'}}>
-                  <Grid item xs={12} sm={8} className="face-wrapper">
-                    <img
-                      src={Face}
-                      alt='Daniel Bocash'
-                      className='face-image'
-                    />
-                    <Paper elevation={3} sx={{width: '50%', marginLeft: "auto", display: "flex", alignItems: "center"}}>
-                      <Typography>
-                        This is hello world
-                      </Typography>
-                    </Paper>
-                  </Grid>
+                <Grid item xs={12} sm={8} className="face-wrapper">
+                  <img
+                    src={Face}
+                    alt='Daniel Bocash'
+                    className='face-image'
+                  />
+                  <Fade in={"true"} timeout={2500}>{introduction}</Fade>
+                </Grid>
               </Paper>
             </Grid>
-            <Grid size={4}>
-              <Skeleton height={100} />
+            <Grid size={4}> 
+              <CustomizedTimeline />
             </Grid>
             <Grid size={8}>
-                <Paper>
-                    About Me
+                <Paper elevation={6} className='about-paper'
+                sx={{
+                  padding: 3,
+                  borderRadius: "16px",
+                  boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.2)",
+                  textAlign: "center",
+                  backgroundColor: "#1976D2",
+                  color: "#fff",
+                  fontWeight: 'bold',
+                }}
+                >
+                  <Typography variant='h5'
+                  sx={{
+                    textAlign: 'center',
+                  }}>
+                    Education
+                  </Typography>
                 </Paper>
                 <Paper 
                 square={false}
                 elevation={0}
+                className='about-desc-paper'
+                sx={{
+                  padding: 2,
+                  borderRadius: "16px",
+                  boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.2)",
+                  textAlign: "center",
+                  backgroundColor: "#1976D2",
+                  color: "#fff"
+                }}
                 >
                     Hello World
                 </Paper>
